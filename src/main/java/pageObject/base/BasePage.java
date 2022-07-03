e1860c1ed764404f8df381aa4c419668f1e843f0
Vladyslav Matsenko <ex65039@gmail.com>
2022-07-03 18:02:54 +0300

    Added language changer

--- src/main/java/pageObject/base/BasePage.java
+++ src/main/java/pageObject/base/BasePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObject.base.enums.Language;
 
 
 import static constants.Constants.TimeoutVariable.EXPLICIT_WAIT;
@@ -14,16 +15,36 @@ import static constants.Constants.TimeoutVariable.EXPLICIT_WAIT;
 public class BasePage {
     protected WebDriver driver;
 
+    @FindBy(css = ".lang__link--active")
+    private WebElement activeLanguage;
+
+    @FindBy(css = "a.lang__link")
+    private WebElement inActiveLanguage;
 
     public BasePage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
 
-
-
     public WebElement waitElementIsVisible(WebElement element){
         new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
         return element;
+    }
+
+    public void changeLanguage(Language lang){
+        switch (lang){
+            case RU:{
+                if (activeLanguage.getText().equalsIgnoreCase("UA")){
+                    inActiveLanguage.click();
+                }
+                break;
+            }
+            case UA:{
+                if (activeLanguage.getText().equalsIgnoreCase("RU")){
+                    inActiveLanguage.click();
+                }
+            }
+            break;
+        }
     }
 }
--- src/test/java/tests/registration/RegistrationTests.java
+++ src/test/java/tests/registration/RegistrationTests.java
@@ -2,6 +2,7 @@ package tests.registration;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pageObject.base.enums.Language;
 import pageObject.rozetka.CatalogMainPage;
 import tests.base.BaseTest;
 
@@ -21,6 +22,7 @@ public class RegistrationTests extends BaseTest{
 
     @Test
     public void verifySuccessfulCustomerRegistration(){
+        catalogMainPage.changeLanguage(Language.UA);
     }
 
 }
