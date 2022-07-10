67fa727db3e49aca2fb133ad8eceda08ae240b12
Vladyslav Matsenko <ex65039@gmail.com>
2022-07-10 12:57:10 +0300

    Upgraded random generator for strings and added test for successful registration

---
+++ src/main/java/pageObject/base/enums/Alphabets.java
@@ -0,0 +1,23 @@
+package pageObject.base.enums;
+
+public enum Alphabets {
+    RU_ALPHABET ("абвгдеёжзийклмнопрстуфхцчшщьъыэюя"),
+    EN_ALPHABET("abcdefghijklmnopqrstuvwxyz"),
+    NUMBERS ("0123456789"),
+    EN_AND_NUMBERS ("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789");
+
+    private String alphabet;
+
+    Alphabets(String alphabet){
+    this.alphabet = alphabet;
+    }
+
+    public String getAlphabet(){
+        return alphabet;
+    }
+
+    @Override
+    public String toString() {
+        return alphabet;
+    }
+}
--- src/test/java/tests/registration/RegistrationTests.java
+++ src/test/java/tests/registration/RegistrationTests.java
@@ -1,12 +1,16 @@
 package tests.registration;
 
 import io.qameta.allure.Description;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pageObject.base.enums.Language;
 import pageObject.base.enums.RandomGenerator;
 import pageObject.rozetka.CatalogMainPage;
 import tests.base.BaseTest;
+
+import java.util.concurrent.TimeUnit;
 
 import static constants.Constants.Urls.ROZETKA_PAGE;
 import static pageObject.base.BasePage.randomGenerator;
@@ -27,7 +31,8 @@ public class RegistrationTests extends BaseTest{
     @Test
     public void verifySuccessfulCustomerRegistration(){
         catalogMainPage.openRegistrationForm()
-                .fillFirstName(randomGenerator(RandomGenerator.FIRST_NAME, 5));
+                .fillRegistrationFormWithValidValues();
+        Assert.assertEquals(catalogMainPage.getTitle(), "Подтверждение номера телефона");
     }
 
 }
