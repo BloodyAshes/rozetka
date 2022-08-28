861d6cbf6384dbe97693879a4cc62ac18ebeed90
Vladyslav Matsenko <ex65039@gmail.com>
2022-08-28 14:34:48 +0300

    Configure

--- src/main/java/driver/WebDriverFactory.java
+++ src/main/java/driver/WebDriverFactory.java
@@ -3,25 +3,33 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class WebDriverFactory {
 
+static String browserForJenkins = System.getenv("BROWSER");
+
     public static WebDriver initDriver(Browsers browser) {
-        switch (browser) {
-            case EDGE: {
-                WebDriverManager.edgedriver().setup();
-                return new EdgeDriver();
-            }
-            case CHROME: {
-                WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
-            }
-            case FIREFOX: {
-                WebDriverManager.firefoxdriver().setup();
-                return new FirefoxDriver();
+        ChromeOptions chromeOptions = new  ChromeOptions();
+        if (browserForJenkins != null) {
+            chromeOptions.getCapability(browserForJenkins);
+        }else {
+            switch (browser) {
+                case EDGE: {
+                    WebDriverManager.edgedriver().setup();
+                    return new EdgeDriver();
+                }
+                case CHROME: {
+                    WebDriverManager.chromedriver().setup();
+                    return new ChromeDriver();
+                }
+                case FIREFOX: {
+                    WebDriverManager.firefoxdriver().setup();
+                    return new FirefoxDriver();
+                }
             }
         }
         return null;
